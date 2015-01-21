4fe7369fcb79975a3148b75d659dc3450ae21c1c
duane & tucker <student@epicodus.com>
2015-01-21 10:41:06 -0800

    add #id spec

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -9,4 +9,11 @@ describe(Train) do
       expect(train.name()).to(eq('blue line'))
     end
   end
+
+  describe('#id') do
+    it("returns the id number of a train line") do
+      train = Train.new({:name => 'blue line', :id => 1})
+      expect(train.id()).to(eq(1))
+    end
+  end
 end
