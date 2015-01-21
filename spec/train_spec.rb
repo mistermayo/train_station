750361e38bb8edeb568c743ac0929ffc308847fe
duane & tucker <student@epicodus.com>
2015-01-21 10:30:00 -0800

    add #name spec

---
+++ spec/train_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('train')
+
+describe(Train) do
+
+  describe('#name') do
+    it("returns the name of the train line") do
+      train = Train.new({:name => 'blue line', id => 1})
+      expect(train.name()).to(eq('blue line'))
+    end
+  end
